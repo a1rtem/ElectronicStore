c2e1865bbeefddb73a5135fda7b4babdbd667bdd
a1rtem <arbapgm@gmail.com>
2022-12-22 21:36:20 -0500

    reformat

--- demo/src/main/java/com/example/JavaFX/StoreApplication.java
+++ demo/src/main/java/com/example/JavaFX/StoreApplication.java
@@ -1,10 +1,6 @@
 package com.example.JavaFX;
 
 import javafx.application.Application;
-import javafx.scene.Group;
-import javafx.scene.Scene;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class StoreApplication extends Application {
@@ -12,12 +8,6 @@ public class StoreApplication extends Application {
     @Override
     public void start(Stage arg0) throws Exception {
         // TODO Auto-generated method stub
-        Group root = new Group();
-        Scene scene = new Scene(root, 600, 600, Color.BLACK);
-        Stage stage = new Stage();
-
-        Text text = new Text();
-        text.setText("a");
     }
 
 }
